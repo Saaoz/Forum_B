b2bf7ae2b0cf9be873c0a91963ea6fe182b50d35
Saaoz <bastie_nb1998@hotmail.fr>
2023-11-11 23:28:06 +0100

    [FEATURE] incrémentation des recherches par username pour active inactive et all

--- Controllers/userController.ts
+++ Controllers/userController.ts
@@ -9,7 +9,7 @@ export const getAllUsers = async (req: Request, res: Response) => {
         res.status(200).json(users);
     } catch (error: unknown) {
         if (error instanceof Error) {
-        res.status(500).json({ error: "Error in getAllUsers: " + error.message });
+            res.status(500).json({ error: "Error in getAllUsers: " + error.message });
         }
     }
 };
@@ -17,12 +17,14 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const getAllActiveUsers = async (req: Request, res: Response) => {
     try {
         const activeUsers = await prisma.user.findMany({
-            where: { is_active: true }
+            where: { is_active: true },
         });
         res.status(200).json(activeUsers);
     } catch (error: unknown) {
         if (error instanceof Error) {
-        res.status(500).json({ error: "Error in getAllActiveUsers: " + error.message });
+            res
+                .status(500)
+                .json({ error: "Error in getAllActiveUsers: " + error.message });
         }
     }
 };
@@ -30,12 +32,14 @@ export const getAllActiveUsers = async (req: Request, res: Response) => {
 export const getAllInactiveUsers = async (req: Request, res: Response) => {
     try {
         const inactiveUsers = await prisma.user.findMany({
-            where: { is_active: false }
+            where: { is_active: false },
         });
         res.status(200).json(inactiveUsers);
     } catch (error: unknown) {
         if (error instanceof Error) {
-        res.status(500).json({ error: "Error in getAllInactiveUsers: " + error.message });
+            res
+                .status(500)
+                .json({ error: "Error in getAllInactiveUsers: " + error.message });
         }
     }
 };
@@ -47,18 +51,18 @@ export const getUserById = async (req: Request, res: Response) => {
         if (user) {
             res.json(user);
         } else {
-            res.status(404).json('User not found by ID');
+            res.status(404).json("User not found by ID");
         }
     } catch (error: unknown) {
         if (error instanceof Error) {
-        res.status(500).json({ error: "Error in getUserById: " + error.message });
+            res.status(500).json({ error: "Error in getUserById: " + error.message });
         }
     }
 };
 
 export const getUserByActiveId = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const onlyActive = req.query.active === 'true';
+    const onlyActive = req.query.active === "true";
     try {
         const user = await prisma.user.findUnique({
             where: { id: Number(id), ...(onlyActive && { is_active: true }) },
@@ -66,18 +70,20 @@ export const getUserByActiveId = async (req: Request, res: Response) => {
         if (user) {
             res.json(user);
         } else {
-            res.status(404).json('Active user not found by ID');
+            res.status(404).json("Active user not found by ID");
         }
     } catch (error: unknown) {
         if (error instanceof Error) {
-        res.status(500).json({ error: "Error in getUserByActiveId: " + error.message });
+            res
+                .status(500)
+                .json({ error: "Error in getUserByActiveId: " + error.message });
         }
     }
 };
 
 export const getUserByInactiveId = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const onlyInactive = req.query.active === 'false';
+    const onlyInactive = req.query.active === "false";
     try {
         const user = await prisma.user.findUnique({
             where: { id: Number(id), ...(onlyInactive && { is_active: false }) },
@@ -85,11 +91,98 @@ export const getUserByInactiveId = async (req: Request, res: Response) => {
         if (user) {
             res.json(user);
         } else {
-            res.status(404).json('Inactive user not found by ID');
+            res.status(404).json("Inactive user not found by ID");
         }
     } catch (error: unknown) {
         if (error instanceof Error) {
-        res.status(500).json({ error: "Error in getUserByInactiveId: " + error.message });
+            res
+                .status(500)
+                .json({ error: "Error in getUserByInactiveId: " + error.message });
+        }
+    }
+};
+
+export const getUserByUsernameActive = async (req: Request, res: Response) => {
+    const { username } = req.params;
+    const onlyActive = req.query.active === "true";
+
+    try {
+        // Construction dynamique de l'objet where
+        let whereClause: { username: string; is_active?: boolean } = { username };
+
+        if (onlyActive) {
+            whereClause.is_active = true;
+        }
+
+        const users = await prisma.user.findMany({
+            where: whereClause,
+        });
+
+        if (users.length > 0) {
+            res.json(users);
+        } else {
+            res.status(404).json("No active users found with the given username");
+        }
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res
+                .status(500)
+                .json({ error: "Error in getUserByUsernameActive: " + error.message });
+        }
+    }
+};
+
+export const getUserByUsernameInactive = async (req: Request, res: Response) => {
+    const { username } = req.params;
+    const onlyInactive = req.query.active === "false";
+
+    try {
+        // Construction dynamique de l'objet where
+        let whereClause: { username: string; is_active?: boolean } = { username };
+
+        if (onlyInactive) {
+            whereClause.is_active = false;
+        }
+
+        const users = await prisma.user.findMany({
+            where: whereClause,
+        });
+
+        if (users.length > 0) {
+            res.json(users);
+        } else {
+            res.status(404).json("No inactive users found with the given username");
+        }
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res
+                .status(500)
+                .json({ error: "Error in getUserByUsernameInactive: " + error.message });
+        }
+    }
+};
+
+export const getUserByUsername = async (req: Request, res: Response) => {
+    const { username } = req.params;
+
+    try {
+        // Construction dynamique de l'objet where
+        let whereClause: { username: string; } = { username };
+
+        const users = await prisma.user.findMany({
+            where: whereClause,
+        });
+
+        if (users.length > 0) {
+            res.json(users);
+        } else {
+            res.status(404).json("No users found with the given username");
+        }
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res
+                .status(500)
+                .json({ error: "Error in getUserByUsername: " + error.message });
         }
     }
 };
