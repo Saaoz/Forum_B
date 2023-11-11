4796dbf63a187455d5c338a909c62bcdc5425f82
Saaoz <bastie_nb1998@hotmail.fr>
2023-11-11 23:02:18 +0100

    [FEATURE] Mise en place d'une route sécurisé qui permettra une vérification du droit / token pour avoir accès à certains pull request

--- Controllers/authController.ts
+++ Controllers/authController.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import passport from 'passport';
 import { User } from '@prisma/client'; 
+import { DecodedToken } from '../Config/types';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'fallbackSecret';
-const expiresIn = '1h';
+
 
 export const loginUser = (req: Request, res: Response) => {
     passport.authenticate('local', { session: false }, (err: Error | null, user: User | false, info: { message: string }) => {
@@ -13,13 +14,40 @@ export const loginUser = (req: Request, res: Response) => {
                 message: info ? info.message : 'Login failed',
             });
         }
+        
+// Générer le token JWT
+            const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' });
 
-        // informations de l'utilisateur à inclure dans le JWT
-        const tokenPayload = { id: user.id, email: user.email };
+// Définir le token dans un cookie HttpOnly
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            //secure en HTTPS only
+            maxAge: 3600000 // Durée de vie du cookie (en millisecondes) 1h
+        });
 
-        const token = jwt.sign(tokenPayload, JWT_SECRET, { expiresIn });
-
-        return res.json({ user, token });
+        return res.status(200).send('Login successful');
     })(req, res);
 };
 
+export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, JWT_SECRET, (err: Error | null, decoded: any) => { 
+            if (err) {
+                return res.status(401).send('Unauthorized');
+            }
+
+            if (decoded) {
+                
+                const decodedToken: DecodedToken = decoded as DecodedToken;
+                req.user = decodedToken;
+                next();
+            } else {
+                res.status(401).send('Invalid token');
+            }
+        });
+    } else {
+        res.status(401).send('No token provided');
+    }
+};
--- Routes/authRoute.ts
+++ Routes/authRoute.ts
@@ -1,9 +1,15 @@
 
 import express from 'express';
-import { loginUser } from '../Controllers/authController';
+import { loginUser, checkAuth } from '../Controllers/authController';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
+
+//la route est à changer checkauth comme son nom l'indique va permettre de vérifié que l'utilisateur et 
+//bien authentifié pour pouvoir request certaines chose comme ces informations perso ou acceder a certaines partie de l'app
+router.get('/protected-route', checkAuth, (req, res) => {
+    res.send('This is a protected route');
+});
 
 export default router;
