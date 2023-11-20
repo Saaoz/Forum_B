d450bc2244141ce577dfed5bbc3fa8761e5d27fa
Saaoz <bastie_nb1998@hotmail.fr>
2023-11-20 23:44:59 +0100

    [OPTI] réduction de la quantité de route avec une gestion par filtre à send du front pour les users , au lieu d'avoir une recherche active et inactive , un filter s'occupe d'afficher seulement les actifs pour les users

--- Routes/categoryRoute.ts
+++ Routes/categoryRoute.ts
@@ -1,20 +1,14 @@
 // category route
 import express from "express";
-import { createCategory, getActiveCategoryById, getAllActiveCategory, getAllCategory, getAllInactiveCategory, getCategoryById, getCategoryByName, getCategoryBynameActive, getCategoryBynameInactive, getInactiveCategoryById, toggleCategoryActiveState, updateCategoryById } from "../Controllers/categoryController";
+import { createCategory, getAllCategory, getCategoryById, getCategoryByName, toggleCategoryActiveState, updateCategoryById } from "../Controllers/categoryController";
 
 const router = express.Router();
 
 router.get('/', getAllCategory);
-router.get('/active', getAllActiveCategory);
-router.get('/inactive', getAllInactiveCategory);
 
 router.get('/:id', getCategoryById);
-router.get('/active/:id', getActiveCategoryById);
-router.get('/inactive/:id', getInactiveCategoryById);
 
 router.get('/:name', getCategoryByName);
-router.get('/:name/active', getCategoryBynameActive);
-router.get('/:name/inactive', getCategoryBynameInactive);
 
 router.patch('/:id/switch', toggleCategoryActiveState);
 
