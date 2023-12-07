884fec8c194ce2b1e3e4d00f7c9ad4cd2c637ecf
Saaoz <bastie_nb1998@hotmail.fr>
2023-12-07 13:24:39 +0100

    [FEATURE] mise en place du blueprint de la table tag avec fichier route et patern du fichier tagController

---
+++ Controllers/tagController.ts
@@ -0,0 +1,118 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+
+const prisma = new PrismaClient();
+
+
+//getAllTag
+
+// export const SSSS = async (req: Request, res:Response) =>{
+//     const {  } = req.body
+//     try{
+
+//     res.status(200).json();
+//     } catch (error) {
+//         if (error instanceof Error) {
+//           res
+//             .status(500)
+//             .json({ error: "Error in : " + error.message });
+//         }
+//       }
+// }
+
+//getAllTagFromTopic
+
+// export const SSSS = async (req: Request, res:Response) =>{
+//     const {  } = req.body
+//     try{
+
+//     res.status(200).json();
+//     } catch (error) {
+//         if (error instanceof Error) {
+//           res
+//             .status(500)
+//             .json({ error: "Error in : " + error.message });
+//         }
+//       }
+// }
+
+//addTagtoTopic
+
+// export const SSSS = async (req: Request, res:Response) =>{
+//     const {  } = req.body
+//     try{
+
+//     res.status(200).json();
+//     } catch (error) {
+//         if (error instanceof Error) {
+//           res
+//             .status(500)
+//             .json({ error: "Error in : " + error.message });
+//         }
+//       }
+// }
+
+//deleteTagFromTopic
+
+// export const SSSS = async (req: Request, res:Response) =>{
+//     const {  } = req.body
+//     try{
+
+//     res.status(200).json();
+//     } catch (error) {
+//         if (error instanceof Error) {
+//           res
+//             .status(500)
+//             .json({ error: "Error in : " + error.message });
+//         }
+//       }
+// }
+
+//UpdateTag
+
+// export const SSSS = async (req: Request, res:Response) =>{
+//     const {  } = req.body
+//     try{
+
+//     res.status(200).json();
+//     } catch (error) {
+//         if (error instanceof Error) {
+//           res
+//             .status(500)
+//             .json({ error: "Error in : " + error.message });
+//         }
+//       }
+// }
+
+//CreateTag
+
+// export const SSSS = async (req: Request, res:Response) =>{
+//     const {  } = req.body
+//     try{
+
+//     res.status(200).json();
+//     } catch (error) {
+//         if (error instanceof Error) {
+//           res
+//             .status(500)
+//             .json({ error: "Error in : " + error.message });
+//         }
+//       }
+// }
+
+//getTop20Tag
+
+// export const SSSS = async (req: Request, res:Response) =>{
+//     const {  } = req.body
+//     try{
+
+//     res.status(200).json();
+//     } catch (error) {
+//         if (error instanceof Error) {
+//           res
+//             .status(500)
+//             .json({ error: "Error in : " + error.message });
+//         }
+//       }
+// }
---
+++ Routes/tagRoute.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+import {  } from '../Controllers/tagController';
+
+const router = express.Router();
+
+
+
+
+//  router.get('/', getAllReply);
+
+// router.get('/topic/:title', getAllReplyFromTopicTitle);
+
+// router.get('/topic/:id', getAllReplyFromTopicId)
+
+// router.get('/user/:id', getAllReplyFromUserId)
+
+// router.post('/topic/:id/create', createReplyForTopic)
+
+// router.patch('/:id/update', updateReply )
+
+// router.patch('/:id/switch_state')
+
+
+
+
+export default router;
