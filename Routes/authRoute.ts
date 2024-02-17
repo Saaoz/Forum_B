aa2a8884bc64b7539894104e3097cc1367c5d181
Saaoz <bastie_nb1998@hotmail.fr>
2024-02-18 00:14:03 +0100

    [FIX UPDATE] réorganisation des routes avec un changement de fichier pour la route de createUser qui passe dans authRoute

--- Routes/authRoute.ts
+++ Routes/authRoute.ts
@@ -1,10 +1,12 @@
 
 import express from 'express';
-import { loginUser, checkAuth } from '../Controllers/authController';
+import { loginUser, checkAuth, createUser } from '../Controllers/authController';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
+
+router.post('/create', createUser);
 
 //la route est à changer checkauth comme son nom l'indique va permettre de vérifié que l'utilisateur et 
 //bien authentifié pour pouvoir request certaines chose comme ces informations perso ou acceder a certaines partie de l'app
--- Routes/userRoute.ts
+++ Routes/userRoute.ts
@@ -1,16 +1,16 @@
 // user route
 import express from "express";
 import { 
-    getAllUsers, getUserById, getUserByUsername, banUserById, debanUserById, upAdminById, revokeAdminById, updateUserById, createUser
+    getAllUsers, getUserById, getUserByUsername, banUserById, debanUserById, upAdminById, revokeAdminById, updateUserById,
 } from '../Controllers/userController';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 
-router.get('/:id', getUserById);
+router.get('/byid/:id', getUserById);
 
-router.get('/:username', getUserByUsername);
+router.get('/byname/:username', getUserByUsername);
 
 router.patch('/:id/ban', banUserById);
 router.patch('/:id/deban', debanUserById);
@@ -19,6 +19,5 @@ router.patch('/:id/admin', upAdminById);
 router.patch('/:id/unadmin', revokeAdminById);
 
 router.patch('/:id/update', updateUserById);
-router.post('/create', createUser);
 
 export default router;
